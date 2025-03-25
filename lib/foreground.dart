16d64752559dbe00d4bb55591c1a286a343bfef5
harrykito <korhary@gmail.com>
2025-03-25 23:05:30 +0900

    update device driver

--- lib/foreground.dart
+++ lib/foreground.dart
@@ -2,6 +2,7 @@
 // UI/UX 핵심 구성요소를 통합하는 핵심 파트입니다.
 import 'package:medi_capstone1/ux.dart';
 import 'package:flutter/material.dart';
+import 'package:medi_capstone1/interface.dart';
 import 'dart:async';
 
 class MainScreen extends StatefulWidget {
@@ -13,11 +14,37 @@ class _MainScreenState extends State<MainScreen> {
   int leftHz = 10;
   int rightHz = 50;
   bool isBlinking = false;
+  final DeviceConnector deviceConnector = DeviceConnector();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('SSVEP Project')),
+      appBar: AppBar(
+        title: Text('SSVEP Project'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              switch (value) {
+                case 'USB':
+                  deviceConnector.connectUSB();
+                  break;
+                case 'Bluetooth':
+                  deviceConnector.connectBluetooth();
+                  break;
+                case 'Wi-Fi':
+                  deviceConnector.connectWiFi("SSID", "password");
+                  break;
+              }
+            },
+            itemBuilder: (context) => [
+              PopupMenuItem(value: 'USB', child: Text('USB 연결')),
+              PopupMenuItem(value: 'Bluetooth', child: Text('Bluetooth 연결')),
+              PopupMenuItem(value: 'Wi-Fi', child: Text('Wi-Fi 연결')),
+            ],
+            icon: Icon(Icons.link),
+          ),
+        ],
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
--- lib/interface.dart
+++ lib/interface.dart
@@ -1,2 +1,30 @@
 // interface.dart
 // 기기와의 통신을 담당하는 클래스 입니다.
+
+// TODO: 필요한 패키지 추가할 것
+// import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+// import 'package:wifi_iot/wifi_iot.dart';
+
+class DeviceConnector {
+  static final DeviceConnector _instance = DeviceConnector._internal();
+  factory DeviceConnector() => _instance;
+  DeviceConnector._internal();
+
+  // FIXME: USB 연결 (구현 필요)
+  Future<void> connectUSB() async {
+    print("USB 연결 시도...");
+  }
+
+  // FIXME: Wi-Fi 연결 (구현 필요)
+  Future<void> connectWiFi(String ssid, String password) async {
+    bool success = false;
+    //await WiFiForIoTPlugin.connect(ssid, password: password);
+    print(success ? "Wi-Fi 연결 성공" : "Wi-Fi 연결 실패");
+  }
+
+  // FIXME: Bluetooth 연결 (구현 필요)
+  Future<void> connectBluetooth() async {
+    // FlutterBluetoothSerial.instance.requestEnable();
+    print("Bluetooth 활성화 요청");
+  }
+}
